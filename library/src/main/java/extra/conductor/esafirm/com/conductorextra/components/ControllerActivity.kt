426dff6ee06fe48948811c720e383070bef40ea6
Esa Firman <esafirm21@gmail.com>
2017-06-05 20:21:52 +0700

    Fix ControllerActivity

--- library/src/main/java/extra/conductor/esafirm/com/conductorextra/components/ControllerActivity.kt
+++ library/src/main/java/extra/conductor/esafirm/com/conductorextra/components/ControllerActivity.kt
@@ -2,25 +2,26 @@ package extra.conductor.esafirm.com.conductorextra.components
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import butterknife.ButterKnife
-import butterknife.Unbinder
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import com.bluelinelabs.conductor.Conductor
+import com.bluelinelabs.conductor.Controller
+import com.bluelinelabs.conductor.RouterTransaction
 
 abstract class ControllerActivity : AppCompatActivity() {
 
-    var unbinder: Unbinder? = null
-
-    abstract fun getLayoutResId(): Int
-    abstract fun onViewBound(savedInstanceState: Bundle?)
+    abstract fun getController(): Controller
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(getLayoutResId())
-        unbinder = ButterKnife.bind(this)
-        onViewBound(savedInstanceState)
-    }
-
-    override fun onDestroy() {
-        unbinder?.unbind()
-        super.onDestroy()
+        setContentView(FrameLayout(this).apply {
+            layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        }.also {
+            Conductor.attachRouter(this@ControllerActivity, it, savedInstanceState).run {
+                if (hasRootController()) {
+                    setRoot(RouterTransaction.with(getController()))
+                }
+            }
+        })
     }
 }
