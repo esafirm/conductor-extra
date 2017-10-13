c8154271c285efaf3a33f4ab65f53975607a3829
Esa Firman <esafirm21@gmail.com>
2017-10-13 14:21:02 +0700

    Complete lifecycle callback

--- conductor-extra/src/main/java/com/esafirm/conductorextra/ControllerExt.kt
+++ conductor-extra/src/main/java/com/esafirm/conductorextra/ControllerExt.kt
@@ -1,5 +1,6 @@
 package com.esafirm.conductorextra
 
+import android.os.Bundle
 import android.view.View
 import com.bluelinelabs.conductor.Controller
 import com.bluelinelabs.conductor.Router
@@ -36,24 +37,55 @@ fun Controller.showDialog(childRouter: Router, controller: Controller) {
 /* > Lifecycle */
 /* --------------------------------------------------- */
 
-typealias Action = Controller.() -> Unit
+typealias Callback<P1, P2> = (P1, P2) -> Unit
+
 fun Controller.addLifecycleCallback(
-        onPreDestroy: Action? = null,
-        onPostAttach: Action? = null,
-        onPreDetach: Action? = null
+        onPreDestroy: ((Controller) -> Unit)? = null,
+        onPostDestroy: ((Controller) -> Unit)? = null,
+        onPostAttach: Callback<Controller, View>? = null,
+        onPostDetach: Callback<Controller, View>? = null,
+        onPreDetach: Callback<Controller, View>? = null,
+        onSaveInstanceState: Callback<Controller, Bundle>? = null,
+        onRestoreInstanceState: Callback<Controller, Bundle>? = null,
+        onSaveViewState: Callback<Controller, Bundle>? = null,
+        onRestoreViewState: Callback<Controller, Bundle>? = null
 ) {
     addLifecycleListener(object : Controller.LifecycleListener() {
 
+        override fun onSaveViewState(controller: Controller, outState: Bundle) {
+            onSaveViewState?.invoke(controller, outState)
+        }
+
+        override fun onRestoreViewState(controller: Controller, savedViewState: Bundle) {
+            onRestoreViewState?.invoke(controller, savedViewState)
+        }
+
+        override fun onRestoreInstanceState(controller: Controller, savedInstanceState: Bundle) {
+            onRestoreInstanceState?.invoke(controller, savedInstanceState)
+        }
+
+        override fun onSaveInstanceState(controller: Controller, outState: Bundle) {
+            onSaveInstanceState?.invoke(controller, outState)
+        }
+
         override fun postAttach(controller: Controller, view: View) {
-            onPostAttach?.run { invoke(controller) }
+            onPostAttach?.invoke(controller, view)
         }
 
         override fun preDetach(controller: Controller, view: View) {
-            onPreDetach?.run { invoke(controller) }
+            onPreDetach?.invoke(controller, view)
+        }
+
+        override fun postDetach(controller: Controller, view: View) {
+            onPostDetach?.invoke(controller, view)
         }
 
         override fun preDestroy(controller: Controller) {
-            onPreDestroy?.run { invoke(controller) }
+            onPreDestroy?.invoke(controller)
+        }
+
+        override fun postDestroy(controller: Controller) {
+            onPostDestroy?.invoke(controller)
         }
     })
 }
