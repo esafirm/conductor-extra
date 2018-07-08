6d1fdbe9db0a554fd1e2affdbfc8061d69c6109f
Esa Firman <esafirm21@gmail.com>
2018-07-08 23:32:07 +0700

    Fix compile error

--- conductor-extra-databinding/src/main/java/com/esafirm/conductorextra/databinding/DataBindingBinder.kt
+++ conductor-extra-databinding/src/main/java/com/esafirm/conductorextra/databinding/DataBindingBinder.kt
@@ -8,7 +8,7 @@ import com.esafirm.conductorextra.components.ControllerBinder
 object DataBindingBinder {
     fun <T : ViewDataBinding> createBinder(): ControllerBinder<T> = object : ControllerBinder<T> {
         override fun bind(view: View): T {
-            return DataBindingUtil.bind(view)
+            return DataBindingUtil.bind(view) ?: throw IllegalStateException("Bound view must not be null")
         }
     }
 }
