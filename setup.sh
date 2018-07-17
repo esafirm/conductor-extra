1dc0091872c412194a4f42e8b7e2ee09f32d9353
Esa Firman <esafirm21@gmail.com>
2018-07-17 13:00:27 +0700

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo "Running setup!"
 rm settings.gradle
 echo "include ':conductor-extra'" > settings.gradle
 echo "include ':conductor-extra-butterknife'" >> settings.gradle
 echo "include ':conductor-extra-databinding'" >> settings.gradle
 echo "include ':conductor-extra-listener'"  >> settings.gradle
+echo "include ':conductor-extra-screen'"  >> settings.gradle
