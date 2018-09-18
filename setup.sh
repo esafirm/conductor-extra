758fc07602585a57b2677c34b8c33327ab9359bd
Esa Firman <esafirm21@gmail.com>
2018-09-18 20:22:17 +0700

    Add common module to setup

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ echo "include ':conductor-extra-butterknife'" >> settings.gradle
 echo "include ':conductor-extra-databinding'" >> settings.gradle
 echo "include ':conductor-extra-listener'"  >> settings.gradle
 echo "include ':conductor-extra-screen'"  >> settings.gradle
+echo "include ':conductor-extra-common'"  >> settings.gradle
